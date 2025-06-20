8dc7003aae708a579a3568e01f4caefabaaf4b60
Afshan <nazneenk@adobe.com>
2025-06-20 15:19:08 +0530

    Css Changes

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,8 +1,1 @@
 // add delayed functionality here
-// async function loadAdobeLaunch() {
-//     if (!scriptEnabled()) { return; }
-  
-//     const config = await fetchPlaceholders(PATH_PREFIX);
-//     const env = config.environment || 'Dev';
-//     await loadScript(config[`analyticsEndpoint${env}`]);
-//   }
